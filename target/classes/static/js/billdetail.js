8f9e8bbe093d2069b79df996419278cde36b6f2a
nileshdoshi991989@gmail.com <nileshdoshi991989@gmail.com>
2017-06-19 19:54:22 +0530

    Correction in search functionality

--- target/classes/static/js/billdetail.js
+++
@@ -1,7 +0,0 @@
-app.controller('billdetailController', ['$scope','$rootScope','$location',function ($scope,$rootScope,$location){
-	$scope.data = $rootScope.selectedBillDetail;
-	
-	if($rootScope.token === undefined){
-		$location.path('/login');
-	}	
-}]);
